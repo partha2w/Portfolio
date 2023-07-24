561cce8b2257895829bfabef048894e7829ae0be
Parthajit Konwar <parthajitkonwar15@gmail.com>
2023-07-24 01:46:24 -0700

    url of project change

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -5,8 +5,8 @@ const WorkCardData = [
         imgsrc: prjctImg1,
         title: "Todolist App",
         text: "Keep track of your daily tasks and stay organized with this user-friendly Todo List app built with React. Plus, the intuitive design makes adding and editing tasks a breeze. Try it out today and take control of your productivity!",
-        view:"https://merntodolist.netlify.app/",
-        source:"https://github.com/partha2w/merntodobackend.git"
+        view:"https://simplereacttodoapp.netlify.app/",
+        source:"https://github.com/partha2w/ReactTodo.git"
      }
 ];
 
